0b99002acc3078b07903e56999b4f05342f7c056
agoenks29D <agungmasda29@gmail.com>
2022-02-10 07:07:53 +0700

    update database

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2022 at 10:22 PM
+-- Generation Time: Feb 10, 2022 at 01:07 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.3.29
 
@@ -18,8 +18,31 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `codeigniter-skripsi`
+-- Database: `pemetaan-kriminal-kmeans`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `desa`
+--
+
+CREATE TABLE `desa` (
+  `id` int(2) NOT NULL,
+  `nama` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dusun`
+--
+
+CREATE TABLE `dusun` (
+  `id` int(3) NOT NULL,
+  `desa` int(2) NOT NULL,
+  `nama` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -34,6 +57,50 @@ CREATE TABLE `email_confirm` (
   `confirm_code` int(6) NOT NULL,
   `expire_date` datetime NOT NULL,
   `status` enum('unconfirmed','confirmed') COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jalan`
+--
+
+CREATE TABLE `jalan` (
+  `id` int(3) NOT NULL,
+  `dusun` int(3) NOT NULL,
+  `nama` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laporan-kriminal`
+--
+
+CREATE TABLE `laporan-kriminal` (
+  `id` int(4) NOT NULL,
+  `nomor_surat` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanggal` date NOT NULL,
+  `jenis` enum('pencurian-motor','pencurian-ringan') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `desa` int(2) NOT NULL,
+  `dusun` int(2) NOT NULL,
+  `jalan` int(3) NOT NULL,
+  `tkp` int(3) DEFAULT NULL,
+  `kerugian_nominal` int(2) NOT NULL,
+  `terjadi_pembunuhan` tinyint(1) NOT NULL DEFAULT 0,
+  `deskripsi` tinytext COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tkp`
+--
+
+CREATE TABLE `tkp` (
+  `id` int(11) NOT NULL,
+  `jalan` int(2) NOT NULL,
+  `nama` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -64,9 +131,33 @@ INSERT INTO `user` (`id`, `role`, `email`, `username`, `password`, `full_name`, 
 --
 
 --
+-- Indexes for table `desa`
+--
+ALTER TABLE `desa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dusun`
+--
+ALTER TABLE `dusun`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `email_confirm`
 --
 ALTER TABLE `email_confirm`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jalan`
+--
+ALTER TABLE `jalan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `laporan-kriminal`
+--
+ALTER TABLE `laporan-kriminal`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -80,10 +171,34 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `desa`
+--
+ALTER TABLE `desa`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dusun`
+--
+ALTER TABLE `dusun`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `email_confirm`
 --
 ALTER TABLE `email_confirm`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jalan`
+--
+ALTER TABLE `jalan`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `laporan-kriminal`
+--
+ALTER TABLE `laporan-kriminal`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
